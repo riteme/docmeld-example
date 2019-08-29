65a2cccc93492448e5bdbc923f72cff2fde452d4
riteme <riteme@qq.com>
2019-08-29 20:52:38 +0800

    Initial commit.

---
+++ hello-world.cpp
@@ -0,0 +1,17 @@
+/**
+ * title: Hello, world!
+ * category: Programming
+ * description: hello-world.md
+ * rank: 666
+ */
+
+#include <iostream>
+
+using namespace std;
+
+#define ACM_BEGIN
+int main() {
+	cout << "Hello, world!" << endl;
+	return 0;
+}
+#define ACM_END
---
+++ infinite-series.md
@@ -0,0 +1,9 @@
+This is a document which is not attached to any C++ source code. Some examples of math formulas rendered by $\KaTeX$:
+
+$$
+\sum_{k = 1}^\infin \frac1k = +\infin
+$$
+
+$$
+\sum_{k = 1}^\infin \frac1{k^2} = \frac{\pi^2}6
+$$
